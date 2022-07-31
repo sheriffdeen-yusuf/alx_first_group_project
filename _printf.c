2cabed46343929bfbe73556fac09f0433bd51882
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-07-31 01:00:10 -0700

    function can return n numbers of char printed

--- _printf.c
+++ _printf.c
@@ -14,19 +14,24 @@ int _printf(const char * const format, ...)
 	
 	va_list args;
 	int i = 0, k = 0;
+	int n_displayed = 0;
 	char *str = NULL;
 	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
+		{
 			_putchar(format[i]);
+			n_displayed++;
+		}
 		else
 		{
 			/* format[i] = % */
 			if (format[i + 1] == 'c')
 			{
 				_putchar(va_arg(args, int));
+				n_displayed++;
 				i++;
 			}
 			else if (format[i + 1] == 's')
@@ -37,6 +42,7 @@ int _printf(const char * const format, ...)
 				while (str[k] != '\0')
 				{
 					_putchar(str[k]);
+					n_displayed++;
 					k++;
 				}
 			}
@@ -44,6 +50,7 @@ int _printf(const char * const format, ...)
 			{
 				i++;
 				_putchar('%');
+				n_displayed++;
 			}
 		}
 		i++;
@@ -51,5 +58,5 @@ int _printf(const char * const format, ...)
 
 	va_end(args);
 
-	return (0);
+	return (n_displayed);
 }
--- test/main.c
+++ test/main.c
@@ -3,7 +3,11 @@
 
 int main()
 {
-	_printf("name is: %s, grade is: %c %%\n", "musa", 'A');
-	printf("name is: %s, grade is: %c %%\n", "musa", 'A');
+	int n = 0;
+	n = _printf("name is: %s, grade is: %c %%\n", "musa", 'A');
+	printf("n: %d\n", n);
+
+	n = printf("name is: %s, grade is: %c %%\n", "musa", 'A');
+	printf("n: %d\n", n);
 	return (0);
 }
