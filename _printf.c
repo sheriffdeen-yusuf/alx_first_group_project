f525faabfb467d1611626306be2fa33a03310ac2
Alexigho <alexbriwen@gmail.com>
2022-08-06 16:33:59 -0700

    initial commit

--- _printf.c
+++ _printf.c
@@ -3,20 +3,22 @@
 #include <stddef.h>
 
 /**
-* printf - accepts arguments and display the number of 
+* _printf - accepts arguments and display the number of
 * arguments passed as parameters.
-*
-* return: number of args
+* @format: the characters to be printed
+* Return: number of args
 */
 
 int _printf(const char * const format, ...)
 {
-	
+
 	va_list args;
 	int i = 0, num = 0;
 	int n_displayed = 0;
 	char *str = NULL;
+
 	va_start(args, format);
+
 	int (*func)(va_list);
 
 	while (format[i] != '\0')
@@ -28,29 +30,12 @@ int _printf(const char * const format, ...)
 		}
 		else
 		{
-			/** This Three lines of code is an improvement of 
-			* the lengthy if else statement that checks a type of
-			* format specifier then calls the appopriate print helper
-			* for the format. 
-			* Now, _select_func is a function pointer to all helper 
-			* function of ssme prototype parameter 'args'. 
-			* it accepts a char, and checks if is a define specifier 
-			* then returns pointer to its print helper function.
-			**/
-
 			func = _select_func(format[i + 1]);
 			if (func != NULL)
 			{
 				n_displayed += func(args);
-				i++;
-				
-			}
-		
-				/** remember, n_displayed is not define in
-				* _printstring(args), hence in correct displayed
-				* count ->  n_displayed++; 
-				*/
-			
+				i++
+
 		}
 		i++;
 	}
