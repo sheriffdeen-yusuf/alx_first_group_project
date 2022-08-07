02d99a121d62a4839dfdfe982456e3257328af7a
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-08-07 14:30:06 -0700

    fix _select func

--- _select_func.c
+++ _select_func.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+* _select_func - accepts a type of va_list and points
+* to a pointer func
+* @c: param its accepts
+* va_list: variable args list
+* Return: a pointer to function
+*/
+
 int (*_select_func(char c))(va_list)
 {
 	if (c == 'c')
