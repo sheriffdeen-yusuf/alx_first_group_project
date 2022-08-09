b73e6fdc7e6214a645500d975147923fcbbfc1ee
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-08-09 04:19:14 -0700

    test II

--- test/main.h
+++ test/main.h
@@ -1,27 +1,54 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <unistd.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define DECIMAL 10
+#define HEX 16
+#define OCTAL 8
+#define BINARY 2
 
 /**
- * struct printer - structure for printing to stdout, given a specifier
- * @specifier: the specifier recognized by this printer (without the %)
- * @run: the function to run when this printer is invoked
+ * struct specifierStruct - struct to hold all specifier
+ * @specifier: character pointer
+ * @func: callback funtion
  */
-typedef struct printer
+typedef struct specifierStruct
 {
 	char *specifier;
-	int (*run)(va_list);
-} printer;
+	int (*func)(const char *, int, va_list, int *);
+} specifierStruct;
 
-printer _get_printer(const char *specifier);
-int _putchar(char c);
+int check_specifier(const char *, int, va_list, int *);
 int _printf(const char *format, ...);
-int _print_char(va_list);
-int _print_str(va_list);
-int _print_decimal(va_list);
-int _print_int(va_list);
-int _print_binary(va_list);
+int isDigit(char);
+int isAlpha(char);
+int setFlags(const char *, int *, int, va_list);
+int print_string(char *, int *, int);
+char *rot13(char *s);
+int get_index(char *, char);
+int _vprintf(const char *, va_list);
+int _printf(const char *, ...);
+void signedNumberToString(int64_t, int, char *, char, int *);
+void unsignedNumberToString(uint64_t, int, char *, char, int *);
+int _putchar(char);
+int format_specifier(int, const char *, va_list, int *, int *,  int *, int *);
+char *reversed(char *);
 
-#endif /* MAIN_H */
+int print_char(const char *, int, va_list, int *);
+int print_int(const char *, int, va_list, int *);
+int print_str(const char *, int, va_list, int *);
+int print_unsigned(const char *, int, va_list, int *);
+int print_addr(const char *, int, va_list, int *);
+int print_oct_bin(const char *, int, va_list, int *);
+int print_rot13(const char *, int, va_list, int *);
+int print_reverse(const char *, int, va_list, int *);
+char *upper_str(char *);
+int isPrintable(char);
+int upper_strlen(char *);
+int getDigitsValue(const char *fmt, int *j, int);
+
+#endif
